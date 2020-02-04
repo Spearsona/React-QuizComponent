00505e3434cfe04b0f10482786d6ace2fad4d7f4
Spearsona <stephen.pearson01@sap.com>
2020-02-04 10:41:44 +0000

    module 7 complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,6 +15,9 @@ class Quiz extends Component{
         })
  
     }
+    handleResetClick(){
+        this.setState({quiz_position: 1 })
+    }
 
     render(){
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
@@ -22,9 +25,11 @@ class Quiz extends Component{
         return (
             <div>
             {
-                isQuizEnd ? <QuizEnd /> : 
+                isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : 
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
-                showNextQuestionHandler = {this.showNextQuestion.bind(this)}/>
+                showNextQuestionHandler = {this.showNextQuestion.bind(this)}
+                resetClickHandler = {this.handleResetClick.bind(this)}
+                />
             }
             </div>
         );
